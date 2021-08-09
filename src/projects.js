d4e6238264307c272ff44a1658ca251ce207573d
shivachit1 <shivachit1@gmail.com>
2021-08-09 20:26:37 +0300

    new country website info

--- src/projects.js
+++ src/projects.js
@@ -1,54 +1,60 @@
-
-const project = [[{
-            projectTitle: "Ajna",
-            projectDescription: "Android, Java, Firebase, Google Maps APIs ",
-            details: "This is a simple chat application that lets users log in with their Gmail account credentials. Users can add other users to their friend's lists, send messages, and make a video call with each other. To bring out the video calling feature in the application, Opentok APIs is used as a medium as it is easy to integrate within the app",
-            imageurl:require('./images/ajna.gif'),
-            info: {
-                devices: "android with api level 26+",
-                demoLink: "https://github.com/shivachit1/Ajna",
-                githubLink:"https://github.com/shivachit1/Ajna"
-                }
-        }
-        , 
-         {
-            projectTitle: "Butterfly",
-            projectDescription: "Unity Engine, Android, C#, Krita, Play Store",
-            details: "This is a simple game developed to learn the building blocks required to create a 2D game in Unity Engine. The game logic is scripted on C# language and the data are stored in Firebase. The game is only available in the play store for android devices. The characters and objects for the game are designed in Paint S and Krita.",
-            imageurl:require('./images/butterfly.gif'),
-            info: {
-                devices: "android with api level 26+",
-                demoLink: "https://play.google.com/store/apps/details?id=com.shiva.butterfly",
-                githubLink:"https://github.com/shivachit1/Butterfly"
-                }
-        }
-    ]
-   ,[
+const project = [
+  [
     {
-        projectTitle: "Frames",
-        projectDescription: "React.js, Redux, CSS, JSON, Netlify for hosting",
-        details: "The project is a demo web-based application for selling glasses frames. Users can view the lists of items, add them to cart, and make an order smoothly. This project is carried out to learn handling an application state management system using Redux. React.js, Redux, CSS, and javaScript are used as building stacks for this website.",
-        imageurl:"https://firebasestorage.googleapis.com/v0/b/portfoliowebsite-8eb68.appspot.com/o/frames.png?alt=media&token=bfefa148-e930-43ba-91b7-759612c019b6",
-        info: {
-            devices: "available via link",
-            demoLink: "https://amazing-kirch-d3995a.netlify.app/",
-            githubLink:"https://github.com/shivachit1/Frames"
-            }
-    
+      projectTitle: "Ajna",
+      projectDescription: "Android, Java, Firebase, Google Maps APIs ",
+      details:
+        "This is a simple chat application that lets users log in with their Gmail account credentials. Users can add other users to their friend's lists, send messages, and make a video call with each other. To bring out the video calling feature in the application, Opentok APIs is used as a medium as it is easy to integrate within the app",
+      imageurl: require("./images/ajna.gif"),
+      info: {
+        devices: "android with api level 26+",
+        demoLink: "https://github.com/shivachit1/Ajna",
+        githubLink: "https://github.com/shivachit1/Ajna",
+      },
     },
     {
-        projectTitle: "EventManager",
-        projectDescription: "Vue.js, Vuex, CSS, Router, JSON, Netlify for hosting",
-        details: "Web-based event managing web application built using Vue.js, Vuex, CSS, Vue router. Users are asked to input their name so that they can create a new event or join any events that are public. In this project, I learned to build a simple website focusing on the frontend using Vue.js and Vuex for managing its whole application state.",
-        imageurl:  'https://firebasestorage.googleapis.com/v0/b/portfoliowebsite-8eb68.appspot.com/o/EventManager.png?alt=media&token=7779e0ab-7da7-49e4-bdf2-a6971188cd0a',
-        info: {
-            devices: "Compatiable to all android devices",
-            demoLink: "https://priceless-bassi-d229f3.netlify.app/#/",
-            githubLink:"https://github.com/shivachit1/EventManager"
-            }
-    }
-    
-   ]
-   
+      projectTitle: "Butterfly",
+      projectDescription: "Unity Engine, Android, C#, Krita, Play Store",
+      details:
+        "This is a simple game developed to learn the building blocks required to create a 2D game in Unity Engine. The game logic is scripted on C# language and the data are stored in Firebase. The game is only available in the play store for android devices. The characters and objects for the game are designed in Paint S and Krita.",
+      imageurl: require("./images/butterfly.gif"),
+      info: {
+        devices: "android with api level 26+",
+        demoLink:
+          "https://play.google.com/store/apps/details?id=com.shiva.butterfly",
+        githubLink: "https://github.com/shivachit1/Butterfly",
+      },
+    },
+  ],
+  [
+    {
+      projectTitle: "Buy Country",
+      projectDescription:
+        "React.js, Redux, Redux Saga, SCSS, JSON, Netlify for hosting",
+      details:
+        "The project is carried out while learning basic building blocks for frontend web application using React.js. Redux, Redux saga, SCSS are few things which are used as a building blocks for this simple e-commerce application.",
+      imageurl:
+        "https://firebasestorage.googleapis.com/v0/b/portfoliowebsite-8eb68.appspot.com/o/countryapp.png?alt=media&token=289c06f9-b558-4272-bb29-0030860b928c",
+      info: {
+        devices: "available via link",
+        demoLink: "hhttps://countries-redux-typescript.netlify.app/",
+        githubLink: "https://github.com/shivachit1/CountryApp",
+      },
+    },
+    {
+      projectTitle: "EventManager",
+      projectDescription:
+        "Vue.js, Vuex, CSS, Router, JSON, Netlify for hosting",
+      details:
+        "Web-based event managing web application built using Vue.js, Vuex, CSS, Vue router. Users are asked to input their name so that they can create a new event or join any events that are public. In this project, I learned to build a simple website focusing on the frontend using Vue.js and Vuex for managing its whole application state.",
+      imageurl:
+        "https://firebasestorage.googleapis.com/v0/b/portfoliowebsite-8eb68.appspot.com/o/EventManager.png?alt=media&token=7779e0ab-7da7-49e4-bdf2-a6971188cd0a",
+      info: {
+        devices: "Compatiable to all android devices",
+        demoLink: "https://priceless-bassi-d229f3.netlify.app/#/",
+        githubLink: "https://github.com/shivachit1/EventManager",
+      },
+    },
+  ],
 ]
-export default project;
+export default project
